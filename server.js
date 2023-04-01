3dded5715e6808f4beba334bf51d24a887ff872d
sdorado2 <sdorado2@gmail.com>
2023-04-01 17:54:31 -0400

    added a view engine

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "jsx");
+app.set("jsx", require("express-react-views"));
+
 app.listen(PORT, () => console.log(`${PORT} is listing!`));
